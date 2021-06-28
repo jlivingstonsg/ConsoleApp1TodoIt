4493a9c4c79c13048d71a0efc701d7eecd979565
jlivingstonsg <magnus.ivarsson@gmail.com>
2021-06-28 17:56:01 +0200

    Add project files.

---
+++ ConsoleApp1TodoIt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1TodoIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
